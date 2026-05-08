e410ce01add0e5b26d903a56b7408bbbebb43af6
jmbchavez <julianmanuelbragachavez@gmail.com>
2026-05-07 21:22:43 -0700

    updated config.js

--- public/config.js
+++ public/config.js
@@ -5,17 +5,9 @@ const BACKEND_HOST = "securechat-group5.onrender.com";
 
 // ---- Do not edit below this line ----
 
-const SecureChatConfig = (() => {
-  const isLocal =
-    location.hostname === "localhost" || location.hostname === "127.0.0.1";
-
-  return {
-    wsUrl: isLocal
-      ? "ws://localhost:8080"
-      : "wss://securechat-group5.onrender.com",
-
-    apiBase: "https://securechatservice.42web.io/api",
-  };
-})();
+const SecureChatConfig = {
+  wsUrl: "wss://securechat-group5-yjem.onrender.com",
+  apiBase: "https://securechatservice.42web.io/api",
+};
 
 window.SecureChatConfig = SecureChatConfig;
